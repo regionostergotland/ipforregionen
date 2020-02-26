9dbc206de95e612a08520fac4e3b58400dd5b1c0
Vanessa Andrea Rojas Serrano <vanro619@student.liu.se>
2020-02-26 14:53:44 +0100

    Add localization demo to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, LOCALE_ID, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wwv';
+  languageList = [
+    { code: 'en', label: 'English'},
+    { code: 'sv', label: 'Svenska'},
+    { code: 'es', label: 'Español'}
+  ];
+  constructor(@Inject(LOCALE_ID) protected localeId: string) {}
 }
