bcef4905ea507d12af03ec20f8a41d69c4d34138
mohom373 <mohom373@student.liu.se>
2020-03-11 12:54:02 +0100

    Added installation manual and settings user story ready for control

--- src/app/gui/settings-view/settings-view.component.ts
+++ src/app/gui/settings-view/settings-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { saveAs } from 'file-saver';
+
 @Component({
   selector: 'app-settings-view',
   templateUrl: './settings-view.component.html',
@@ -9,10 +10,12 @@ export class SettingsViewComponent implements OnInit {
   settingsJSON: object = {};
   constructor() { }
 
+  file: File;
+
   ngOnInit() {
   }
 
-  saveToFile() {
+  saveToFile() : void{
     if (JSON.parse(localStorage.getItem("destination_names")).length > 0 
         && JSON.parse(localStorage.getItem("destination_urls")).length > 0) {
           let namesArray = JSON.parse(localStorage.getItem("destination_names"));
@@ -36,5 +39,26 @@ export class SettingsViewComponent implements OnInit {
     let fileName = "backup_" + timeForm.toISOString() + ".json";
 
     saveAs(blob, fileName);
+  }
+
+  uploadToApp(file) : void{
+    this.file = file.target.files[0];
+    console.log(this.file.name);
+    let result;
+
+    let reader = new FileReader();
+    reader.readAsText(this.file, "UTF-8");
+    reader.onload = (evt: Event) => {
+      result = reader.result;
+      result = JSON.parse(result);
+      console.log(typeof result);
+      console.log(result);
+
+      if (!!result["destinations"]) {
+        let destinations = result["destinations"];
+        localStorage.setItem('destination_names', JSON.stringify(destinations["names"]));
+        localStorage.setItem('destination_urls', JSON.stringify(destinations["urls"]));
+      }
+    }    
   }
 }
