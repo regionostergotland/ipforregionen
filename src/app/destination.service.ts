128ab7207e34f245a744979c0fb3d16b02a49816
Agnes Hallberg <agnha531@liu.student.se>
2020-04-22 12:08:39 +0200

    Added destination service

---
+++ src/app/destination.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { DataList } from './ehr/datalist';
+
+/*@Injectable({ <-- Fungerar inte för att Destination konstruktor tar parametrar.
+  providedIn: 'root' 
+})*/
+/**
+* Destination determines what data should be sent to which destination.
+* Contains destination's name, url and data to be sent.
+*/
+export class Destination {
+  private name : string;
+  private url : string;
+  private categories : Map<string, DataList>;
+
+  constructor(name:string, url:string) 
+  {
+    this.name = name;
+    this.url = url;
+    this.categories = new Map<string, DataList>(); 
+  };
+
+  public getDestinationName(): string {
+    return this.name;
+  }
+
+  public setDestinationName(name: string) {
+    this.name = name;
+  }
+
+  public getDestinationUrl(): string {
+    return this.url;
+  }
+
+  public setDestinationUrl(url: string) {
+    this.url = url;
+  }
+
+  public getCategories(): Map<string, DataList> {
+    return this.categories;
+  }
+
+  public setCategories(categories : Map<string, DataList> ){
+    this.categories = categories;
+  }
+
+  public setDataList(categoryId: string, list: DataList) {
+    this.categories.set(categoryId, list);
+  }
+}
