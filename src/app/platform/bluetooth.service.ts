756b75d4015da833c5c511a60a7ae0522fdecee8
Vanessa Andrea Rojas Serrano <vanro619@student.liu.se>
2020-05-12 11:46:41 +0200

    Pulsoximeter reading gets value 31 every time - not the expected value

--- src/app/platform/bluetooth.service.ts
+++ src/app/platform/bluetooth.service.ts
@@ -13,7 +13,9 @@ import { Categories,
   providedIn: 'root'
 })
 export class BluetoothService extends Platform {
-  private device: BluetoothDevice;
+  device: BluetoothDevice;
+  characteristic: BluetoothCharacteristicUUID;
+  navigator: Navigator;
   static GATT_PRIMARY_SERVICE = 'pulse_oximeter';
 
   constructor(
@@ -32,6 +34,22 @@ export class BluetoothService extends Platform {
 
   }
 
+/*  public async requestDevice() {
+    console.log("[X] Requesting Bluetooth Device...");
+    this.device = await navigator.bluetooth.requestDevice({
+      //acceptAllDevices: true,
+      filters: ['battery_service']
+    });
+    this.device.addEventListener('gattserverdisconnected', onDisconnected);
+  }
+
+  public async connectDevice() {
+    console.log("[X] Connecting to device...");
+    console.log(this.device);
+    console.log('> DeviceName:             ' + device.name);
+    console.log('> DeviceId:               ' + device.id);
+  }*/
+
   /**
   * Attempts to pair with a Bluetooth device
   */
@@ -48,8 +66,9 @@ export class BluetoothService extends Platform {
       console.log('Requesting Bluetooth Device ...')
       const device = await (window.navigator as any).bluetooth.requestDevice({
         // Filters to limit the pairing options showed
-        filters: [{services: serviceUuid }],
-        optionalServices: ['generic_access']
+        acceptAllDevices: true,
+        //filters: [{services: serviceUuid }],
+        optionalServices: [serviceUuid]
       });
       console.log(device);
       console.log('> DeviceName:             ' + device.name);
@@ -63,24 +82,40 @@ export class BluetoothService extends Platform {
       const service = await server.getPrimaryService(serviceUuid);
 
       //this is a characteristic specific for pulse oximeters
-      const characteristic = await service.getCharacteristic("plx_spot_check_measurement");
+      let characteristic = await service.getCharacteristic("plx_continuous_measurement");
+
       console.log(characteristic);
+
       console.log('> Characteristic UUID:  ' + characteristic.uuid);
       console.log('> Broadcast:            ' + characteristic.properties.broadcast);
       console.log('> Read:                 ' + characteristic.properties.read);
-      //console.log('> Write w/o response:   ' + characteristic.properties.writeWithoutResponse);
-      //console.log('> Write:                ' + characteristic.properties.write);
+      console.log('> Write w/o response:   ' + characteristic.properties.writeWithoutResponse);
+      console.log('> Write:                ' + characteristic.properties.write);
       console.log('> Notify:               ' + characteristic.properties.notify);
       console.log('> Indicate:             ' + characteristic.properties.indicate);
-      //console.log('> Signed Write:         ' + characteristic.properties.authenticatedSignedWrites);
-      //console.log('> Queued Write:         ' + characteristic.properties.reliableWrite);
-      //console.log('> Writable Auxiliaries: ' + characteristic.properties.writableAuxiliaries);
+      console.log('> Signed Write:         ' + characteristic.properties.authenticatedSignedWrites);
+      console.log('> Queued Write:         ' + characteristic.properties.reliableWrite);
+      console.log('> Writable Auxiliaries: ' + characteristic.properties.writableAuxiliaries);
 
+      //let value = await characteristic.readValue();
+      //console.log("> Value is " +  value.getUint8(0) + ".");
+
+      await characteristic.startNotifications();
+      characteristic.addEventListener('characteristicvaluechanged',
+            this.handleValueChanged);
+
+      console.log("We're done");
+      //await characteristic.stopNotifications();
     } catch(error) {
       console.log('That did not work! ' + error);
     }
   }
 
+  handleValueChanged(event) {
+    let value = event.target.value.getUint8(0);
+    console.log('> Value is ' + value + '.');
+
+  }
 
   public signOut(): void {}
 
